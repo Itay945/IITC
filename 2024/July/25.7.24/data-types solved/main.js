4356571d75d5ee95f3b98ac8384622a304339897
Itay Benadon <itay945@yahoo.com>
2024-07-26 19:01:21 +0300

    yes

---
+++ 2024/July/25.7.24/data-types solved/main.js
@@ -0,0 +1,232 @@
+////////////////////////////////// task 1
+
+
+// x = 5 is a basic number
+
+
+//////////////////////////////// task 2
+
+
+// you push either let or const and declare a string
+
+
+/////////////////////////////// task 3
+
+//null does not exist and undefined can get a definition
+//he eventually will be defined.
+
+
+
+///////////////////////////// task 4
+
+console.log(typeof 3);
+
+
+
+
+////////////////////////////// task 5
+
+//mathematics calculations
+
+let calculation = (5 / 2)
+    console.log(calculation);
+
+
+
+
+
+
+//////////////////////////////////// task 6
+
+
+//boolean expression, its either on or off
+
+
+
+
+/////////////////////////////// task 7
+
+
+
+let user = {
+    firstName: "itay",
+    lastName: "benadon",
+    isStudent: true,
+    age: 24
+
+
+}
+console.log(user.age);
+//can search for specific data on said object with object.keyvalue
+
+
+//////////////////////////// task 8
+
+
+// not a number, always turns out false
+
+//////////////////////////// task 9
+
+////// === means strictly equels to
+
+
+let man = (false == 0)
+console.log(man);
+
+
+
+let man_1 = (false === 0)
+console.log(man_1);
+
+
+
+
+////////////////////////////// task 10
+
+
+let number = 6
+let secondNumber = (6).toString()
+console.log(secondNumber);
+
+
+//////////////////////////// task 11
+
+
+console.log(typeof("5" + 3));
+
+////// string
+
+
+///////////////////////// task 12
+
+
+
+console.log(typeof(3));
+
+
+////////////using typeof operator
+
+
+
+
+
+/////////////////////////// task 13
+
+
+
+let string = `multiline string toooooool
+multiline string toooooool
+multiline string toooooool
+multiline string toooooool
+multiline string toooooool`
+console.log(string);
+
+
+
+////////////////////////// task 14
+
+
+
+
+// let string_1 = 'string tool'
+// let string_2 = "string tool 2"
+// let string_3 = `string tool tree`
+
+
+
+//////////////////////////// task 15
+
+
+
+let numberToString_1 = (5).toString()
+console.log(numberToString_1);
+
+
+
+///////////////////////////// task 16
+
+
+
+
+console.log(typeof null);
+//////// an object. it suggests it may have value later.
+
+
+
+//////////////////////////// task 17
+
+
+const allTheTime = 0
+
+
+///////////using const
+
+
+////////////////////////////// task 18
+
+
+
+
+let addedValue = (18 + "18")
+console.log(addedValue);
+////////////it adds them ontop of each other as a string
+
+
+////////////////////////////////// task 19
+
+
+
+let trial = (10 > 6)
+console.log(trial);
+
+
+
+
+
+/////////////////////////////////// task 20
+
+
+
+const text = "are we humans? or \"are we dancers?\""
+console.log(text);
+
+
+
+///////////////////////////////// task 21
+
+////var is older, so we shall not use it.
+/// let is a variable and it's value could change
+/// const is constant and his value could not be changed
+
+
+////////////////////////////////// task 22
+
+
+
+let something;
+console.log(typeof something);
+
+
+
+//////////////////////////// task 23
+//////// it becomes a string
+
+
+let somethingElse = (10 + "5")
+console.log(somethingElse);
+///it makes them a string and concatenates them
+
+
+
+
+//////////////////////////// task 24
+
+
+let isTrue = (2 > 1)
+
+
+////////////////////////// task 25
+
+
+//// primitive data value once set cannot be changed.
+//// and objects can have other functions to them.
---
+++ 2024/July/25.7.24/string methods solved/main.js
@@ -0,0 +1,233 @@
+//////////////////////////// task 1
+
+function length(string) {
+let number = string.length 
+return number
+} 
+console.log(length("itay"))
+///////////////////////// task 2
+
+function upper(hihihaha) {
+let hohohaha = hihihaha.toUpperCase()
+return hohohaha
+} 
+console.log(upper("yes"));
+
+
+//////////////////////// task 3
+
+
+
+function lower(yesyes) {
+    let something = yesyes.toLowerCase()
+return something
+}
+console.log(lower("NONO"))
+
+
+////////////////////////////// task 4
+
+
+
+function specificIndex(string){
+    let specific = string.charAt(3)
+    return specific
+}
+console.log(specificIndex("Eran"));
+
+//////////////////////////// task 5
+
+
+
+function extractedPortion(first) {
+    let portion = first.substring(1, 4)
+    return portion
+}
+console.log(extractedPortion("benadon"));
+///////////////// returns letters from first position 1
+///////////////// up until 4
+
+
+
+
+
+///////////////////////////// task 6
+
+
+function replacingWord(word) {
+    let replaced = word.replace("benadon", "abutbul")
+    return replaced
+} 
+console.log(replacingWord("itay benadon!"));
+
+
+//////////////////////////// task 7
+
+
+
+function  removeString(string) {
+    let remove = string.trim()
+    return remove
+}
+console.log(removeString("   trim me b****    "));
+
+
+
+//////////////////////////// task 8
+
+
+
+function startWith(string) {
+    let start = string.startsWith("do")
+    return start
+}
+console.log(startWith("do you remember me?"));
+
+
+
+////////////////////////////// task 9
+
+
+function stringEndWith(string){
+let ends = string.endsWith("no?")
+return ends
+}
+console.log(stringEndWith("can you do this for me or no?"));
+
+
+
+//////////////////////////// task 10
+
+
+
+
+function searchString(string){
+let search = string.indexOf("yes")
+return search
+}
+console.log(searchString("Well yes indeed."));
+
+
+
+///////////////////////////////////////// task 11
+
+
+
+function splitForArray(string){
+    let splitting = string.split("hero", "villain")
+    return splitting
+}
+console.log(splitForArray("superman"));
+/////////////////////////////////////********** need help */
+
+
+
+
+
+//////////////////////////////////////// task 12
+
+
+function stringRepeat(string) {
+    let repeat = string.repeat(3)
+    return repeat
+}
+console.log(stringRepeat("what? "));
+
+
+
+
+/////////////////////////////////////// task 13
+
+
+
+
+function concatation(string, sString){
+let together = string.concat(sString)
+return together
+}
+console.log(concatation("for ", "real?"));
+
+
+
+
+////////////////////////////////////// task 14
+
+
+
+function stringPad(string){
+    let something = string.padStart(1, 4)
+    return something
+}
+console.log(stringPad(" benadon "));
+//////////////////////////////unsure of its usage.
+
+
+
+
+
+////////////////////////////////////////// task 15
+
+
+function stringSlice(string){
+    let something = string.slice(0, 5)
+    return something
+}
+console.log(stringSlice("benadon"));
+
+
+
+
+/////////////////////////////////////// task 16
+
+
+
+
+function stringReplace(string, sString, tString){
+    let something = string.replace(sString, tString)
+    return something
+}
+console.log(stringReplace("benadon went to the mall today."));
+////////////////////////////////////////////// didn't understand.
+
+
+
+
+////////////////////////////////// task 17
+
+function stringIsIncludes(string){
+    let something = string.includes("Y")
+    return something
+}
+console.log(stringIsIncludes("You know what i talk about?"));
+
+////////////////////////////////// task 18
+
+
+function stringLegnth(string){
+    return string.length
+}
+console.log(stringLegnth("benadon"));
+
+
+
+//////////////////////////////// task 19
+
+
+
+
+function stringLegnth_1(string){
+   let length = string.length
+   if (length === 0) return "is 0"
+   else return "is more then 0"
+   // return  length
+}
+console.log(stringLegnth_1("benadon"));
+
+
+
+//////////////////////////////// task 20
+
+
+
+///////////////need help to understand slice
+
