3605fa8ef9796f740caf2f029378a1aace592cc2
Itay Benadon <itay945@yahoo.com>
2024-07-26 10:39:08 +0300

    addedFiles

---
+++ 2024/July/25.7.24/variables solved/main.js
@@ -0,0 +1,213 @@
+/////////////////////////////// task 1
+
+let firstName = "itay"
+/////////////////////////////// task 2
+
+let age = "24"
+
+/////////////////////////////// task 3
+
+
+let isRaining = true
+
+
+////////////////////////////// task 4
+
+
+
+const PI = 3.14159
+
+
+///////////////////////////// task 5
+
+
+
+let isStudent = true
+
+
+console.log(isStudent)
+//////////////////////////// task 6
+
+
+let favoriteColor;
+
+
+
+
+//////////////////////////// task 7
+
+
+
+let tremperature = 72.5
+
+//////////////////////////// task 8
+
+const DAY_IN_WEEK = 7
+
+/////////////////////////// task 9
+
+let username = "coder123"
+
+
+///////////////////////// task 10
+
+
+let number = 0
+
+
+//////////////////////// task 11
+
+
+
+let isLoggedIn = false
+
+
+
+
+//////////////////////// task 12
+
+
+
+const COMPANY_NAME ="Tech Solutions Inc."
+
+
+
+/////////////////////////// task 13
+
+
+
+const date = new Date();
+console.log(date.getFullYear());
+
+
+
+
+///////////////////////// task 14
+
+
+
+
+
+let price = 19.99
+
+
+
+
+/////////////////////////// task 15
+
+
+
+
+
+
+let quantity = 100
+
+
+
+
+
+
+///////////////////////////// task 16
+
+
+
+let counter = 0
+counter ++;
+
+counter ++;
+
+console.log(counter);
+
+
+
+/////////////////////////// task 17
+
+
+
+let massage = "Hello"
+
+let fullMassage = (massage + ", " + "world")
+
+console.log(fullMassage);
+
+
+
+/////////////////////////// task 18
+    
+
+let myVariable = 42
+myVariable = ("forty-two")
+
+console.log(myVariable)
+//////unsure***************
+
+
+
+
+
+//////////////////////// task 19
+
+
+
+let totalCost = 50
+let addedCost =+ (totalCost + 10)
+console.log(addedCost);
+
+
+///////////////////////////// task 20
+
+
+username = "user1"
+username = "admin"
+
+
+
+
+///////////////////////////task 21
+
+
+let temperature = 20
+let fahrenheit = ([temperature * 1.8] + 32)
+
+
+console.log(fahrenheit);
+
+///////////////////////////// task 22
+
+
+
+
+const fruits = ["apple, banana"]
+const extrafruit = fruits.push("orange")
+console.log(fruits);
+
+
+
+//////////////////////////// task 23
+
+
+let person = "phone"
+person = "rehovot"
+
+console.log(person);
+
+
+
+
+////////////////////////////////// task 24
+
+
+
+let cows = 5
+cows-= 1
+console.log(cows);
+
+
+
+//////////////////////////////////// task 25
+
+
+
+let greeting = "Good"
+let together = (greeting + " " + "morning")
+console.log(together);
