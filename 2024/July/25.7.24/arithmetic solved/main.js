8dca21fe2e3f55a0ab327e2777e046ee2d725977
Itay Benadon <itay945@yahoo.com>
2024-07-26 23:15:40 +0300

    sendit

---
+++ 2024/July/25.7.24/arithmetic solved/main.js
@@ -0,0 +1,183 @@
+///////////////////////// task 1
+
+
+console.log(5 + 3);
+
+
+
+//////////////////////// task 2
+
+
+let number_1 = 5
+let number_2 = 6
+let together = (number_1 + number_2)
+console.log(together);
+
+
+
+
+
+///////////////////////// task 3
+
+
+console.log(7 - 5);
+
+
+
+
+/////////////////////// task 4
+
+
+
+
+console.log(10 - 4);
+
+
+
+
+//////////////////////// task 5
+
+
+
+console.log(10 * 5);
+
+
+
+
+/////////////////////// task 6
+
+
+
+
+console.log(6 * 2);
+
+
+
+
+/////////////////////// task 7
+
+
+
+console.log(5 / 2);
+
+
+
+
+///////////////////// task 8
+
+
+
+
+console.log(15 / 3);
+
+
+
+
+
+//////////////////// task 9
+
+
+
+console.log(10 / 0);
+//infinity
+
+///////////////////// task 10
+
+
+
+
+console.log(10 % 3);
+
+
+
+
+//////////////////// task 11
+
+
+
+
+console.log(17% 5);
+
+
+
+
+
+//////////////////// task 12
+
+
+
+
+let x = 5
+x++
+x++
+x++
+x++
+console.log(x);
+
+
+
+//////////////////////// task 13
+
+
+
+
+///////////They achieve the same result but one is shorter
+
+
+
+
+//////////////////////// task 14
+
+
+
+
+let y = 5
+y--
+y--
+console.log(y);
+
+x -= 5
+
+console.log(x);
+
+
+
+//////////////////////////// task 15
+
+// with pemdas
+// (2*5) + 10
+let result = 10 + 5 * 2
+console.log(result);
+
+
+
+/////////////////////////// task 16
+
+/// using ()
+// (2*5) + 10
+
+
+
+///////////////////////// task 17
+
+let numberResult = (10 + 5) * 2
+console.log([10 + 5] * 2);
+
+
+/////////////////////////// task 18
+let number_s = "5"
+let number_n = + number_s
+console.log(number_n);
+
+
+////////////////////////// task 19
+
+
+console.log("5" + 3);
+
+
+///////////////////////// task 20
+
+
+
+console.log("10" - 5);
