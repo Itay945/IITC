4892a532305ce10dba66051747a715992001f0b3
Itay Benadon <itay945@yahoo.com>
2024-07-28 12:29:09 +0300

    y

---
+++ 2024/July/28.7.24/main.js
@@ -0,0 +1,35 @@
+////////////////////// task 1
+// for (let i = 10; i > 0; i--)
+//     console.log(i);
+
+
+///////////////////// task 2
+
+
+// for (let i = 2; i <= 20; i += 2){
+//     console.log(x);
+// }
+    
+///////////////////// task 3
+
+// let sum = 0 
+// for (let i = 1; i <= 10; i++ ) {
+//     sum += i
+//    ;
+// }
+//     console.log(sum);
+////////////// notice the scope////////
+
+
+//////////////////// task 4
+
+
+// for (let i = 0; i <= 50; i += 5){
+//     console.log(i);
+// }
+
+////////////////// task 5
+
+
+for (i = 0; i <=5; i++)
+    console.log("*".repeat(i));
