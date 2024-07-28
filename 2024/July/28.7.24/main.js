920d28a286c9660e8a656b81d0ebb65001218857
Itay Benadon <itay945@yahoo.com>
2024-07-28 16:30:57 +0300

    y

--- 2024/July/28.7.24/main.js
+++ 2024/July/28.7.24/main.js
@@ -31,5 +31,48 @@
 ////////////////// task 5
 
 
-for (i = 0; i <=5; i++)
-    console.log("*".repeat(i));
+// for (i = 0; i <=5; i++)
+//     console.log("*".repeat(i));
+////// can use string inside the loop using .repeat()
+
+
+
+/////////////////
+
+// let i = 1
+// while (i < 100) {
+//     console.log(i);
+//     i *= 2
+    
+// }
+    
+// let i = 1;
+// do {
+//     console.log(i); ////consol.log(i++)
+//     i++;
+// } 
+// while  (i <= 5)
+
+
+///////////////////////////////////// practice time
+////////////////// task 1
+
+
+
+let fFoods = ["Pizza", "Icecream", "Tasbihim", "Fried chicken", "Pomelo"]
+console.log(fFoods[2]);
+
+fFoods[1] = "Avocado"
+console.log(fFoods[1]);
+
+fFoods.push("Bread")
+console.log(fFoods);
+
+fFoods.shift();
+console.log(fFoods);
+
+for (let i = 0; i < fFoods.length; i++)
+    console.log(fFoods[i]);
+
+let index = fFoods.indexOf("Fried chicken")
+console.log(index);
