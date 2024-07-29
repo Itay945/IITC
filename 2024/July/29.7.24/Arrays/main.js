3ddc6c2c86ecb0d3caa83549dbed2fab3bbfe0c3
Itay Benadon <itay945@yahoo.com>
2024-07-29 12:22:48 +0300

    go

---
+++ 2024/July/29.7.24/Arrays/main.js
@@ -0,0 +1,64 @@
+/// js array practice exercises
+
+///////////////////////////////////// practice time
+////////////////// task 1
+
+
+
+// let fFoods = ["Pizza", "Icecream", "Tasbihim", "Fried chicken", "Pomelo"]
+// console.log(fFoods[2]);
+
+// fFoods[1] = "Avocado"
+// console.log(fFoods[1]);
+
+// fFoods.push("Bread")
+// console.log(fFoods);
+
+// fFoods.shift();
+// console.log(fFoods);
+
+// for (let i = 0; i < fFoods.length; i++)
+//     console.log(fFoods[i]);
+
+// let index = fFoods.indexOf("Fried chicken")
+// console.log(index);
+
+// let arr = [1,2,3,4,5]
+// let sum = 0
+// for (i = 0; i < arr.length; i++)
+//     sum+=arr[i]
+//     console.log(sum);
+
+
+
+////////////////////////// Array Exercises
+////////////////////// task 1-3
+let fruits = []
+fruits.push("Pomelo")
+let lastItem = fruits.pop()
+fruits[1] = "kiwi"
+console.log(fruits);
+
+
+
+
+let nums = [1,2,3,4,5]
+console.log(nums[2]);
+
+
+
+let colors = []
+colors.push("red","green","blue")
+colors.shift()
+colors.unshift("yellow")
+console.log(colors);
+////// task 9
+let sum = 0
+let numbs = [1,2,3,4,5]
+for (i = 0; i < numbs.length; i++)
+    sum+=numbs[i]
+    console.log(sum);
+
+/// task 10
+
+console.log(colors.indexOf("green");
