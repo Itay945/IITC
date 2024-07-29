6c377e2fc9513c8c186a1f05f45df7d549f312dd
Itay Benadon <itay945@yahoo.com>
2024-07-29 11:26:44 +0300

    update

---
+++ 2024/July/29.7.24/Loops/main.js
@@ -0,0 +1,100 @@
+/////////////////// basic counting exercises
+
+
+////////////////// task 1
+
+
+// for (i = 0; i <= 5; i++)
+//     console.log(i);
+
+
+//////////////////// task 2
+
+// for (i = 0; i <= 9; i++)
+//     console.log(i)
+
+
+/////////////////// task 3
+
+
+// for (i = 10; i >= 1; i--)
+//     console.log(i)
+
+
+/////////////////// task 4
+
+
+// for (i = 0; i <= 10; i++)
+//     if (i % 2 === 0)
+//     console.log(i);
+
+
+/////////////////// task 5
+
+// for (i = 0; i <= 10; i++)
+//     if (i % 2 === 1)
+//     console.log(i);
+
+////////////////// task 6
+
+// for (i = 0; i <= 15; i+=3)
+//     console.log(i);
+
+
+////////////////// task 7
+
+// for (i = 1; i <= 20; i+=2)
+// console.log(i);
+
+
+////////////////// task 8
+
+// for (i = 2; i <= 20; i+=2)
+//     console.log(i);
+
+////////////////// task 9
+
+// for (i = 10; i >= 0; i-=2)
+//     console.log(i);
+
+
+////////////////// task 10
+
+
+// for (i = 5; i <= 25; i+=5)
+//     console.log(i);
+
+
+////////////////// task 11
+
+// let dog = ""
+// for (i = 0; i <= 5; i++)
+//     dog += "*"
+//     console.log(dog);
+    //////////unsure was a bit confusing
+
+
+////////////////// task 12
+
+// for (let i = 0; i < 3; i++) {
+//     console.log("Hello");
+// }
+
+
+///////////////// task 13
+
+
+// for (let i = 1; i <= 3; i++)
+//     console.log(i,"!");
+
+//////////////////// task 14
+
+// let arr = ["a","b","c","d","e"]
+// for (i = 0; i < arr.length; i++)
+//     console.log(arr[i]);
+
+//////////////////// task 15
+
+for (i = 2; i <= 4; i++)
+    i = "2"
+  console.log(i);
---
+++ 2024/July/29.7.24/Number and Math Methods/main.js
@@ -0,0 +1,123 @@
+/////////////////////// task 1
+let x = 4.7
+let y = Math.round(x)
+console.log(y);
+
+
+///////////////////// task 2
+
+let xS = 4
+let yS = Math.sqrt(xS)
+console.log(yS);
+
+
+///////////////////// task 3
+
+let y_2 = Math.pow(2,3)
+console.log(y_2);
+
+
+//////////////////// task 4
+
+let x_3 = -16
+let y_3 = Math.abs(x_3)
+console.log(y_3);
+
+
+/////////////////// task 5
+
+let x_4 = 5.4444444444
+let y_4 = Math.floor(x_4)
+console.log(y_4);
+
+/////////////////// task 6
+
+let x_5 = 6.66666666666
+let y_5 = Math.ceil(x_5)
+console.log(y_5);
+
+
+/////////////////// task 7
+
+let r = Math.random()
+console.log(r);
+
+
+/////////////////// task 8
+let uno = Math.max(5, 10, 15)
+console.log(uno);
+
+/////////////////// task 9
+
+
+
+
+
+let dos = Math.min(5, 10, 15)
+console.log(dos);
+
+
+/////////////////// task 10
+
+
+let sin = Math.sin(90)
+console.log(sin);
+
+////////////////// task 11
+
+let cos = Math.cos(0)
+console.log(cos);
+
+/////////////////// task 12
+
+
+
+let tan = Math.tan(45)
+console.log(tan);
+
+
+/////////////////// task 13
+
+let yY = Math.log(2)
+console.log(yY);
+
+////////////////// task 14
+
+
+let cC = Math.exp(3)
+console.log(cC);
+
+
+
+////////////////// task 15
+
+let kK = 3.7
+let gG = Math.trunc(kK)
+console.log(gG);
+
+////////////////// task 16
+
+
+let rNumber = (Math.random() * 10 + 1) 
+let fNumber = Math.floor(rNumber)
+console.log(fNumber);
+
+
+
+////////////////// task 17
+
+
+let smth = 0.5
+let smth_1 = Math.asin(smth)
+console.log(smth_1);
+
+
+/////////////////// task 18
+
+let b = Math.hypot(3, 4)
+console.log(b);
+
+
+/////////////////// task 19
+
+
