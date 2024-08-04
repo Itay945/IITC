ff6a0ffe77316dbeda27f2e6838c0460ba9c7083
Itay Benadon <itay945@yahoo.com>
2024-08-04 22:54:40 +0300

    hihihaha

---
+++ 2024/august/4.8.24/main.js
@@ -0,0 +1,58 @@
+// let students = [
+//     {id: "1", name: "omer"},
+//     {id: "2", name: "avi"},
+//     {id: "3", name: "gaga"},
+// ];
+
+
+// function getArrayByKeys(array, key) {
+//     let firstName = []
+//     for (let i = 0; i < array.length; i++) {
+//    firstName.push(array[i][key])
+    
+        
+//     } 
+//     return firstName;
+// }getArrayByKeys(students, "name"))
+
+
+
+
+// let students = [
+//     {id: "1", name: "omer", grade: 100},
+//     {id: "2", name: "avi", grade: 40},
+//     {id: "3", name: "gaga", grade: 80},
+// ];
+
+
+// function getPassStudents(array, grade) {
+//     let newArray = []
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i].grade > grade )
+//        newArray.push(array[i]["grade"])
+         
+        
+        
+//     }return newArray
+    
+    
+// }console.log(getPassStudents(students, 80));
+ 
+
+
+
+let people = ["yossi","liraz","baba"]
+
+
+function createPeople(names) {
+    let people = []
+    for (let i = 0; i < names.length; i++) {
+       people.push({ name: names[i]})
+        
+       people.push(newguy)
+    }
+
+
+
+    return people
+} console.log(createPeople(["yossi","liraz","baba"]));
