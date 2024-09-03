c3f0f0325bb93d9060ef0898e13375512246be8a
Itay Benadon <itay945@yahoo.com>
2024-09-03 19:43:58 +0300

    y

--- 2024/September/2.9.24/main.js
+++ 2024/September/2.9.24/main.js
@@ -38,28 +38,78 @@
 // groupOccurences()
 
   
-const nestedArray = [[1, 2, 3], [4, [5, 6]], [7, 8], [9]];
-const flatArr = []
+// const nestedArray = [[1, 2, 3], [4, [5, 6]], [7, 8], [9]];
+// const flatArr = []
 
-  for (let i = 0; i < nestedArray.length; i++) {
-    if (!Array.isArray(nestedArray[i])) {
-      flatArr.push(nestedArray[i])
-    }
-    else
-    for (let j = 0; j < nestedArray[i].length; j++) {
-      if (!Array.isArray(nestedArray[i][j])) {
-        flatArr.push(nestedArray[i][j])
+//   for (let i = 0; i < nestedArray.length; i++) {
+//     if (!Array.isArray(nestedArray[i])) {
+//       flatArr.push(nestedArray[i])
+//     }
+//     else
+//     for (let j = 0; j < nestedArray[i].length; j++) {
+//       if (!Array.isArray(nestedArray[i][j])) {
+//         flatArr.push(nestedArray[i][j])
       
-    }
-    else
-    for (let k = 0; k < nestedArray[i][j].length; k++) {
-      flatArr.push(nestedArray[i][j][k])
+//     }
+//     else
+//     for (let k = 0; k < nestedArray[i][j].length; k++) {
+//       flatArr.push(nestedArray[i][j][k])
       
-    }
+//     }
     
       
+//     }
+    
+    
+//     }console.log(flatArr);
+  
+
+// const numberGrid = [
+//   [3, 5, 1],
+//   [9, 7, 2],
+//   [6, 8, 4]
+// ];
+
+
+// function calculateArray(arr) {
+
+//   let sum = 0
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//       sum+= arr[i][j]
+      
+      
+//     }
+    
+    
+    
+   
+    
+//   }return sum
+// }
+
+
+
+// console.log(calculateArray(numberGrid))
+
+
+
+const numberGrid = [
+  [15, 29, 12],
+  [48, 33, 21],
+  [5, 7, 26]
+];
+
+let max = numberGrid[0][0]
+
+for (let i = 0; i < numberGrid.length; i++) {
+  for (let j = 0; j < numberGrid[i].length; j++) {
+    if (max < numberGrid[i][j]) {
+      max = numberGrid[i][j]
     }
     
-    
-    }console.log(flatArr);
+  }
+  
+  
+}console.log(max);
   
