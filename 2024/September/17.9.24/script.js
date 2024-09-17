b1f17ad686da14161ef90752e00b1567b29f91a7
Itay Benadon <itay945@yahoo.com>
2024-09-17 16:09:53 +0300

    yeyeye

--- 2024/September/17.9.24/script.js
+++ 2024/September/17.9.24/script.js
@@ -1,9 +1,27 @@
 const content = document.getElementById("content");
 content.style.backgroundColor="lightblue";
-content.style.color="lightred"
+content.style.color="red"
 const liContent = document.querySelectorAll("li")
     
 
 for (let i = 0; i < liContent.length; i++) {
     console.log(liContent[i].textContent);
 }
+
+const myBtn = document.getElementById("button")
+
+const list = document.getElementById("list")
+myBtn.addEventListener("click", function (){
+    list.style.backgroundColor="yellow"
+    
+})
+const parant = document.getElementById("parant")
+const newLi = document.createElement("li")
+newLi.textContent = "New Item"
+parant.appendChild(newLi)
+
+const addItem = document.getElementById("addItem")
+addItem.addEventListener("click" function (ev.){
+    
+})
+
