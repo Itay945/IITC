a081cde547cf5f96f7515600600639e80dc2c02d
Itay Benadon <itay945@yahoo.com>
2024-09-29 21:45:30 +0300

    yeah

---
+++ 2024/September/29.9.24/main.js
@@ -0,0 +1,49 @@
+// const array = [1, 2, 3, 4, 5]
+
+// array.forEach(num => {
+//    let number = num * 2
+//     console.log(number);
+    
+// })
+
+// const array = [1, 2, 3, 4, 5]
+
+// array.forEach(num => {
+//    let number = num * 2
+//     console.log(number);
+    
+// })
+
+// const array = [1, 2, 3, 4, 5]
+// let secondArray = []
+// array.forEach(num => {
+//    secondArray.push(num)
+    
+    
+// })
+// console.log(secondArray);
+
+// const array = [1, 2, 3, 4, 5]
+// let sum = 0
+// array.forEach(num => {
+//     sum+=num
+// })
+// console.log(sum);
+
+// const array = [1, 2, 3, 4, 5]
+// numbers = []
+// array.forEach(num => {
+    
+//   number = num ** 2
+//   numbers.push(number)
+// })
+// console.log(numbers);
+
+
+const arr = [1, 2, 3, 4, 5]
+
+array = arr.map(num => {
+    numbers = num * 2
+    return numbers
+})
+console.log(array);
