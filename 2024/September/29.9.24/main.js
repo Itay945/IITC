adf01e5db49abf1a1917ab13c6d0e5b6df62a61a
Itay Benadon <itay945@yahoo.com>
2024-10-01 16:47:16 +0300

    y

--- 2024/September/29.9.24/main.js
+++ 2024/September/29.9.24/main.js
@@ -2,6 +2,8 @@
 
 // const array = [1, 2, 3, 4, 5]
 const domView = document.getElementById("my-dom")
+
+// domView.innerHTML = `<p>${array}</p>`
 // array.forEach(num => {
 //    let number = num * 2
 //     console.log(number);
@@ -41,49 +43,107 @@ const domView = document.getElementById("my-dom")
 // })
 // console.log(numbers);
 
-['Hello', ' ', 'World', '!'].
-
-array
-
 // const arr = [1, 2, 3, 4, 5]
 
 // array = arr.map(num => {
-//     numbers = num * 2    
-//     return numbers
+    //     numbers = num * 2    
+    //     return numbers
+    // })
+    // console.log(array);
+    
+
+    
+//   const array =  ['Hello', ' ', 'World', '!']
+//     empt = ""
+//     array.forEach(string => {
+//         empt += string
+
+        
+//     })
+
+// console.log(empt);
+
+
+
+
+
+// const array = ['תפוח', 'בננה', 'דובדבן']
+
+// const newArr = array.map(str =>{
+//     const speed = str.length
+//     return speed
+// })
+// console.log(newArr);
+
+
+// const array = [1, 4, 9, 16, 25]
+
+// const smth = array.map(str => {
+//    const num = str ** 2
+//     return num
+// })
+// console.log(smth);
+
+
+// const arr = ['שלום', 'עולם']
+
+// const array = arr.map(str =>{
+//     smth = str.toUpperCase()
+//     return smth
 // })
 // console.log(array);
 
 
 
+// const arr = [true, false, true]
+// const array = arr.map(bool => {
+//     smth = !bool
+//     return smth
+// })
+// console.log(array);
 
 
+//  const arr = [1, 2, 3, 4, 5, 6]
+
+//     const array = arr.filter(num => {
+        
+//     if (num % 2 === 0) {
+//         return num
+//     }
+//     else {
+//         return ""
+//     }
+        
+       
+//     })
+//     console.log(array);
+    
 
 
+// const arr = ['תפוח', 'בננה', 'דובדבן', 'תמר', 'אלדרברי']
 
 
+// const array = arr.filter(num => num.length > 5)
+// console.log(array);
 
 
+// //13
+// const arr = [5, 10, 15, 20, 25]
+
+// const array = arr.filter(num => num > 10)
+
+// console.log(array);
 
 
+// const arr = ['תפוח', 'בננה', 'תפוז', 'דובדבן']
+
+// const array = arr.filter(string => string.startsWith("ת"))
+// console.log(array);
 
 
+// const arr = [1, 2, 3, 4, 5, 6]
 
+// const array = arr.filter(num => num % 2 === 0)
+// console.log(array);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-domView.innerHTML = `<p>${array}</p>`
+//16
