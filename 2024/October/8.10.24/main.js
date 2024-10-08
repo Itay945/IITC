10064510de1ae8f4cdae38c2801481764894f6e6
Itay Benadon <itay945@yahoo.com>
2024-10-08 16:13:13 +0300

    y

--- 2024/October/8.10.24/main.js
+++ 2024/October/8.10.24/main.js
@@ -18,3 +18,62 @@
 //    }, 1000)
 // }
 
+
+// function solution(cb) {
+//     cb()
+// }
+
+// const cbfn = () =>  {
+//     console.log("Hallo World");
+    
+// }
+
+// solution(cbfn)
+
+
+// const smth = ((cb, fName) =>    {
+//     cb(fName)
+// })
+// const dpMsg = (string) => {
+//     console.log(`hello ${string}`);
+    
+// }
+// smth(dpMsg,"LIAV")
+//https://github.com/public-apis/public-apis
+const imgDiv = document.querySelector(".imageDiv");
+
+
+let smth = fetch("https://dog.ceo/api/breeds/image/random")
+.then(response => response.json())
+.then(data => {
+    
+    insertImage(data.message);
+});
+
+function insertImage(src) {
+    imgDiv.insertAdjacentHTML("afterbegin", `
+        <img src="${src}" alt="Dog Image">
+        `);
+    }
+    insertImage(smth);
+
+
+
+
+
+const cats = document.querySelector(".cat-fact");
+
+
+let that = fetch("https://cat-fact.herokuapp.com/fact")
+.then(response => response.json())
+.then(data => {
+    
+    insertImage(data.message);
+});
+
+function insertImage(src) {
+    imgDiv.insertAdjacentHTML("afterbegin", `
+        <p src="${src}">
+        `);
+    }
+    insertImage(smth);
