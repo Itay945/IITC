c4aa36d2f323f2284582df107961e04b7a97fce2
Itay Benadon <itay945@yahoo.com>
2024-10-10 14:28:21 +0300

    y

--- 2024/projects/Frontend mentor/faq-accordion-main/main.js
+++ 2024/projects/Frontend mentor/faq-accordion-main/main.js
@@ -1,9 +1,29 @@
-    const plus = document.querySelectorAll(".plus-asset")
-    const text = document.querySelectorAll(".explanation")
-    console.log(text);
-    plus.forEach((plusS, index) => {
-        plusS.addEventListener("click", () =>{
-            plusS.classList.add('hidden');
-            text[index].classList.remove("hidden")
-        })
+    // const plus = document.querySelectorAll(".plus-asset")
+    // const text = document.querySelectorAll(".explanation")
+    // console.log(text);
+    // plus.forEach((plusS, index) => {
+    //     plusS.addEventListener("click", () =>{
+    //         plusS.classList.add('hidden');
+    //         text[index].classList.remove("hidden")
+    //     })
+    // });
+
+    const plus = document.querySelectorAll(".plus-asset");
+const minus = document.querySelectorAll(".minus-asset");
+const text = document.querySelectorAll(".explanation");
+
+plus.forEach((plusS, index) => {
+    plusS.addEventListener("click", () => {
+        plusS.classList.add('hidden'); 
+        minus[index].classList.remove('hidden');
+        text[index].classList.remove("hidden");
     });
+});
+
+minus.forEach((minusS, index) => {
+    minusS.addEventListener("click", () => {
+        minusS.classList.add('hidden');
+        plus[index].classList.remove('hidden'); 
+        text[index].classList.add("hidden"); 
+    });
+});
