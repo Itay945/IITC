fb8124a07deef460033c39a9d85536b2c7f8093b
Itay Benadon <itay945@yahoo.com>
2024-10-11 11:06:20 +0300

    done

--- 2024/October/10.10.24/anotherone/main.js
+++ 2024/October/10.10.24/anotherone/main.js
@@ -1,5 +1,5 @@
 console.log("main.js");
-
+let API_KEY = '590f80257a1ed7e74491347b0c44a419'
 fetch(`https://api.openweathermap.org/data/2.5/weatherlat=39.099724&lon=-94.578331&appid=${API_KEY}`)
   .then(response => response.json())
   .then(data => console.log(data))
