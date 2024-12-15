d9743213e18f5d6c8b331f4f6855ccc88304a1bf
Itay Benadon <itay945@yahoo.com>
2024-12-15 11:50:38 +0200

    y

--- 2024/December/15.12.24/Jokes API/app.js
+++ 2024/December/15.12.24/Jokes API/app.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
-mongoose.connect(process.env.MONGODB_URI).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
    console.log("Connected to MongoDB");
 });
 
