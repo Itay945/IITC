75c49c98e448e5a3ddfa80985ab15fe7bbc66ba8
Itay Benadon <itay945@yahoo.com>
2024-12-25 09:21:25 +0200

    yeah

---
+++ 2024/December/24.12.24/vite-project/src/components/PokemonContainer.jsx
@@ -0,0 +1,8 @@
+import { useState, userEffect } from "react";
+const PokemonContainer = () => {
+  <div>
+    <h1>this is Pokemon Container</h1>
+  </div>;
+};
+
+export default PokemonContainer;
--- 2024/December/24.12.24/vite-project/src/pages/HomePage.jsx
+++ 2024/December/24.12.24/vite-project/src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
+// Components
+import PokemonContainer from "../components/PokemonContainer";
 const HomePage = () => {
   return (
     <>
       <h1>This is Home Page </h1>
       <p>some text here</p>
+      <PokemonContainer />
     </>
   );
 };
