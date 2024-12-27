280b41711683b47fc56a0ddf1facc14dfbf46759
Itay Benadon <itay945@yahoo.com>
2024-12-27 16:52:47 +0200

    y

--- 2024/December/26.12.24/25.12.2024/Client/src/main.jsx
+++ 2024/December/26.12.24/25.12.2024/Client/src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import axios from "axios";
+import "./App.css";
+import RandomJoke from "./Components/RandomJokes.jsx";
+import JokesList from "./Components/JokesList.jsx";
 
-createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <App />
-  // </StrictMode>,
-)
+function Main() {
+  const [joke, setJoke] = useState("");
+  const [jokes, setJokes] = useState([]);
+
+  async function fetchJoke() {
+    const res = await axios.get("http://localhost:3000/api/v1/jokes/random");
+    setJoke(res.data);
+  }
+
+  async function getAllJokes() {
+    const res = await axios.get("http://localhost:3000/api/v1/jokes");
+    setJokes(res.data);
+  }
+
+  useEffect(() => {
+    fetchJoke();
+    getAllJokes();
+  }, []);
+
+  return (
+    <div>
+      <RandomJoke joke={joke} fetchJoke={fetchJoke} />
+      <JokesList jokes={jokes} />
+    </div>
+  );
+}
+
+// Render the Main component
+ReactDOM.createRoot(document.getElementById("root")).render(<Main />);
