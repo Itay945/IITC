3b6753524c64d5776b49645234e6b21067f90eb1
Itay Benadon <itay945@yahoo.com>
2025-01-05 20:25:54 +0200

    y

--- 2024/january/5.1.25/tusks-list/src/App.tsx
+++ 2024/january/5.1.25/tusks-list/src/App.tsx
@@ -1,16 +1,13 @@
-import CreateTaskButton from './components/create-task-button';
-import TaskTable from './components/task-table';
-
-
+import CreateTaskButton from "./components/create-task-button";
+import TaskTable from "./components/task-table";
 
 function App() {
   return (
-    <div className='flex items-center justify-center min-h-screen bg-blue-100 '>
+    <div className="flex  flex-col items-center justify-center min-h-screen bg-blue-100 ">
       <TaskTable />
       <CreateTaskButton />
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
--- 2024/january/5.1.25/tusks-list/src/components/create-task-button.tsx
+++ 2024/january/5.1.25/tusks-list/src/components/create-task-button.tsx
@@ -1,7 +1,7 @@
-
-export default  function CreateTaskButton() {
+export default function CreateTaskButton() {
   return (
-    <button>New Task</button>
-  )
+    <div className="flex justify-center items-center h-7 w-20 bg-gray-100 rounded tracking-tighter mt-2 border border-black">
+      <button>New Task</button>
+    </div>
+  );
 }
-
